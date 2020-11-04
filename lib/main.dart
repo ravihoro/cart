b6369d0934b7a42f1044656695975b7481f357a5
ravihoro <ravijohnhoro5@gmail.com>
2020-11-04 20:17:28 +0530

    Added google sign

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:cart/screens/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import './screens/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MyApp(),
   );
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:cart/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,12 +9,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  signOut() async {
+    await GoogleSignIn().signOut();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLoggedIn', false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Cart',
+        ),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Sign Out"),
+          onPressed: () async {
+            await signOut();
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => LoginPage()));
+          },
         ),
       ),
     );
